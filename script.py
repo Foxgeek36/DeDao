a15af5be9f964729cc2c15e8cbf89c48f0420a6e
Foxgeek36 <1002301246@qq.com>
2019-08-27 23:48:13 +0800

    second commit

--- script.py
+++ script.py
@@ -1,13 +1,20 @@
+# coding=utf-8
 import json
 import pymongo
 from mitmproxy import ctx
 
+# attention +--
 client = pymongo.MongoClient('localhost')
-db = client['igetget']
+db = client['dedao']
 collection = db['books']
 
 
 def response(flow):
+    '''
+    使用mitmproxy获取dedao_app数据接口信息的测试操作
+    :param flow: HTTPflow对象
+    :return:
+    '''
     global collection
     url = 'https://dedao.igetget.com/v3/discover/bookList'
     if flow.request.url.startswith(url):
@@ -21,5 +28,6 @@ def response(flow):
                 'summary': book.get('other_share_summary'),
                 'price': book.get('price')
             }
+            # attention the log setting
             ctx.log.info(str(data))
             collection.insert(data)
